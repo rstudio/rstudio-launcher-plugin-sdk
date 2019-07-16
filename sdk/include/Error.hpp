c60ec155f291c2af025ebeb7ee66e37328d98cbc
Maria Semple <maria@rstudio.com>
2019-07-16 01:13:44 -0700

    flesh out Error in more detail based on the IDE impl.

--- sdk/include/Error.hpp
+++ sdk/include/Error.hpp
@@ -10,28 +10,208 @@
 #ifndef LAUNCHER_PLUGINS_ERROR_HPP
 #define LAUNCHER_PLUGINS_ERROR_HPP
 
+#include <boost/core/noncopyable.hpp>
+
+#include <boost/system/error_code.hpp>
+
 #include <string>
+#include <memory>
 
 namespace rstudio {
 namespace launcher_plugins {
 
+class Error;
+class Success;
+
 /**
- * @brief Class which represents an error.
+ * @brief A class which can be derived from in order to prevent child classes from being derived from further.
  */
-class Error
+class Error_Lock
+{
+   // Classes which may derive this class.
+   friend class Error;
+   friend class Success;
+
+private:
+   /**
+    * @brief Private constructor to prevent further derivation of Error and Success.
+    */
+   Error_Lock() = default;
+};
+
+/**
+ * @brief Class which represents the location of an error
+ */
+class ErrorLocation : boost::noncopyable
 {
 public:
+   /**
+    * @brief Default constructor.
+    */
+   ErrorLocation();
+
+   /**
+    * @brief Move constructor.
+    *
+    * @param in_other   The error location to move to this.
+    */
+   ErrorLocation(ErrorLocation&& in_other) noexcept;
+
+   /**
+    * @brief Constructor.
+    *
+    * param in_function     The function in which the error occurred.
+    * @param in_file        The file in which the error occurred.
+    * @param in_line        The line at which the error occurred.
+    */
+   ErrorLocation(const char* in_function, const char* in_file, long in_line);
+
+   /**
+    * @brief Checks whether the location is set.
+    *
+    * @return True if a location has been set; false otherwise.
+    */
+   bool hasLocation() const;
+
+   /**
+    * @brief Gets the function where the error occurred.
+    *
+    * @return The function where the error occurred.
+    */
+   const std::string& getFunction() const;
+
+   /**
+    * @brief Gets the file where the error occurred.
+    *
+    * @return The file where the error occurred.
+    */
+   const std::string& getFile() const;
+
+   /**
+    * @brief Gets the line where the error occurred.
+    *
+    * @return The line where the error occurred.
+    */
+   long getLine() const;
+
+   /**
+    * @brief Formats the error location as a string.
+    *
+    * @return The error location formatted as a string.
+    */
+   std::string asString() const;
+
+private:
+   // The private implementation of ErrorLocation.
+   struct Impl;
+   std::unique_ptr<Impl> m_impl;
+};
+
+/**
+ * @brief Class which represents an error.
+ *
+ * This class should not be derived from since it is returned by value throughout the SDK. Instead, create helper
+ * functions for each "sub-class" of Error that would be desired.
+ *
+ * Errors are not copyable. To return by value, use std::move.
+ */
+class Error : virtual Error_Lock, public boost::noncopyable
+{
+public:
+   /**
+    * @brief Default constructor.
+    */
+   Error();
+
+   /**
+    * @brief Move constructor.
+    */
+   Error(Error&& in_other) noexcept;
+
+   /**
+    * @brief Constructor.
+    *
+    * @param in_ec              The boost error code.
+    * @param in_errorLocation   The location of the error.
+    */
+   Error(const boost::system::error_code& in_ec, ErrorLocation in_errorLocation);
+
+   /**
+    * @brief Constructor.
+    *
+    * @param in_ec              The boost error code.
+    * @param in_cause           The error which caused this error.
+    * @param in_errorLocation   The location of the error.
+    */
+   Error(const boost::system::error_code& in_ec, Error in_cause, ErrorLocation in_errorLocation);
+
+   /**
+    * @brief Constructor.
+    *
+    * @param in_ec              The boost error code.
+    * @param in_errorMessage    The detailed error message. (e.g. "The JobNetworkRequest is not supported by this plugin.")
+    * @param in_errorLocation   The location of the error.
+    */
+   Error(const boost::system::error_code& in_ec, std::string in_errorMessage, ErrorLocation in_errorLocation);
+
+   /**
+    * @brief Constructor.
+    *
+    * @param in_ec              The boost error code.
+    * @param in_errorMessage    The detailed error message. (e.g. "The JobNetworkRequest is not supported by this plugin.")
+    * @param in_cause           The error which caused this error.
+    * @param in_errorLocation   The location of the error.
+    */
+   Error(const boost::system::error_code& in_ec, std::string in_errorMessage, Error in_cause, ErrorLocation in_errorLocation);
+
+   /**
+    * @brief Constructor.
+    *
+    * @param in_errorCode        The error code.
+    * @param in_name             A contextual name of the error code. (e.g. "RequestNotSupported")
+    * @param in_errorLocation    The location of the error.
+    */
+   Error(int in_errorCode, std::string in_name, ErrorLocation in_errorLocation);
+
+   /**
+    * @brief Constructor.
+    *
+    * @param in_errorCode        The error code.
+    * @param in_name             A contextual name of the error code. (e.g. "RequestNotSupported")
+    * @param in_cause            The error which caused this error.
+    * @param in_errorLocation    The location of the error.
+    */
+   Error(int in_errorCode, std::string in_name, Error in_cause, ErrorLocation in_errorLocation);
+
    /**
     * @brief Constructor.
     *
     * @param in_errorCode            The error code. (e.g. 1)
-    * @param in_errorDescription     A contextual description of the error code. (e.g. "RequestNotSupported")
+    * @param in_errorName            A contextual name of the error code. (e.g. "RequestNotSupported")
     * @param in_errorMessage         The detailed error message. (e.g. "The JobNetworkRequest is not supported by this plugin.")
+    * @param in_errorLocation        The location of the error.
     */
-   explicit Error(
-      int in_errorCode = 0,
-      std::string in_errorDescription = "",
-      std::string in_errorMessage = "");
+   Error(
+      int in_errorCode,
+      std::string in_errorName,
+      std::string in_errorMessage,
+      ErrorLocation in_errorLocation);
+
+   /**
+    * @brief Constructor.
+    *
+    * @param in_errorCode            The error code. (e.g. 1)
+    * @param in_errorName            A contextual name of the error code. (e.g. "RequestNotSupported")
+    * @param in_errorMessage         The detailed error message. (e.g. "The JobNetworkRequest is not supported by this plugin.")
+    * @param in_cause                The error which caused this error.
+    * @param in_errorLocation        The location of the error.
+    */
+   Error(
+      int in_errorCode,
+      std::string in_errorName,
+      std::string in_errorMessage,
+      Error in_cause,
+      ErrorLocation in_errorLocation);
 
    /**
     * @brief Virtual destructor for inheritance.
@@ -39,21 +219,66 @@ public:
    virtual ~Error() = default;
 
    /**
-    * @brief Overload operator bool to allow Errors to be treated as boolean values.
+    * @brief Overloaded operator bool to allow Errors to be treated as boolean values.
     *
     * @return True if there is an error; false otherwise.
     */
-   explicit operator bool();
+   operator bool() const;
+
+   /**
+    * @brief Formats the error as a string.
+    *
+    * @return The error formatted as a string.
+    */
+   std::string asString() const;
+
+   /**
+    * @brief Formats a summary of the error as a string.
+    *
+    * @return A summary of the error.
+    */
+   std::string getSummary() const;
+
+   /**
+    * @brief Gets the error code.
+    *
+    * @return The error code.
+    */
+   int getErrorCode() const;
+
+   /**
+    * @brief Gets the name of the error.
+    *
+    * @return The name of the error.
+    */
+   const std::string& getName() const;
+
+   /**
+    * @brief Gets the error message.
+    *
+    * @return The error message.
+    */
+   const std::string& getMessage() const;
+
+   /**
+    * @brief Gets the cause of the error.
+    *
+    * @return The cause of the error.
+    */
+   const Error& getCause() const;
+
+   /**
+    * @brief Gets the location where the error occurred.
+    *
+    * @return The location where the error occurred.
+    */
+   const ErrorLocation& getLocation() const;
+
 
 private:
-   // The error code.
-   int m_errorCode;
-
-   // The error description.
-   std::string m_errorDescription;
-
-   // The error message.
-   std::string m_errorMessage;
+   // The private implementation of Error.
+   struct Impl;
+   std::unique_ptr<Impl> m_impl;
 };
 
 /**
@@ -64,8 +289,56 @@ class Success: public Error
    /**
     * @brief Constructor.
     */
-   Success() { };
+   Success() : Error() { };
 };
+
+
+/**
+ * @brief Function which creates a system error.
+ *
+ * @param in_errorCode        The error code.
+ * @param in_errorLocation    The location of the error.
+ *
+ * @return A system error.
+ */
+Error systemError(int in_errorCode, ErrorLocation in_location);
+
+/**
+ * @brief Function which creates a system error..
+ *
+ * @param in_errorCode            The error code. (e.g. 1)
+ * @param in_cause                The error which caused this error.
+ * @param in_errorLocation        The location of the error.
+ *
+ * @return A system error.
+ */
+Error systemError(int in_errorCode, Error in_cause, ErrorLocation in_location);
+
+/**
+ * @brief Function which creates a system error..
+ *
+ * @param in_errorCode            The error code. (e.g. 1)
+ * @param in_errorMessage         The detailed error message. (e.g. "Failed to open socket while attempting to connect to Kubernetes.")
+ * @param in_errorLocation        The location of the error.
+ *
+ * @return A system error.
+ */
+Error systemError(int in_errorCode, std::string in_errorMessage, ErrorLocation in_location);
+
+/**
+ * @brief Function which creates a system error..
+ *
+ * @param in_errorCode            The error code. (e.g. 1)
+ * @param in_errorMessage         The detailed error message. (e.g. "Failed to open socket while attempting to connect to Kubernetes.")
+ * @param in_cause                The error which caused this error.
+ * @param in_errorLocation        The location of the error.
+ *
+ * @return A system error.
+ */
+Error systemError(int in_errorCode, std::string in_errorMessage, Error in_cause, ErrorLocation in_location);
+
+#define ERROR_LOCATION rstudio::launcher_plugins::ErrorLocation( \
+   BOOST_CURRENT_FUNCTION,__FILE__,__LINE__)
 
 } // namespace launcher_plugins
 } // namespace rstudio
--- sdk/src/Error.cpp
+++ sdk/src/Error.cpp
@@ -9,21 +9,313 @@
 
 #include "Error.hpp"
 
+#include <ostream>
+#include <sstream>
+
 namespace rstudio {
 namespace launcher_plugins {
 
-Error::Error(int in_errorCode, std::string in_errorDescription, std::string in_errorMessage) :
-   m_errorCode(in_errorCode),
-   m_errorDescription(std::move(in_errorDescription)),
-   m_errorMessage(std::move(in_errorMessage))
+// Error Location ======================================================================================================
+struct ErrorLocation::Impl
+{
+   Impl() : Line(0) { };
+
+   Impl(const char* in_function, const char* in_file, long in_line) :
+      Function(in_function),
+      File(in_file),
+      Line(in_line)
+   { }
+
+   std::string Function;
+   std::string File;
+   long Line;
+};
+
+std::ostream& operator<<(std::ostream& in_os, const ErrorLocation& in_location)
+{
+   in_os << in_location.getFunction() << " "
+         << in_location.getFile() << ":"
+         << in_location.getLine() ;
+
+   return in_os;
+}
+
+ErrorLocation::ErrorLocation() :
+   m_impl(new ErrorLocation::Impl())
+{
+}
+
+ErrorLocation::ErrorLocation(ErrorLocation&& in_other) noexcept :
+   m_impl(std::move(in_other.m_impl))
+{
+}
+
+ErrorLocation::ErrorLocation(const char* in_function, const char* in_file, long in_line) :
+   m_impl(new ErrorLocation::Impl(in_function, in_file, in_line))
+{
+}
+
+bool ErrorLocation::hasLocation() const
+{
+   return m_impl->Line != 0;
+}
+
+const std::string& ErrorLocation::getFunction() const
+{
+   return m_impl->Function;
+}
+
+const std::string& ErrorLocation::getFile() const
+{
+   return m_impl->File;
+}
+
+long ErrorLocation::getLine() const
+{
+   return m_impl->Line;
+}
+
+std::string ErrorLocation::asString() const
+{
+   std::ostringstream oss;
+   oss << *this;
+   return oss.str();
+}
+
+
+// Error ==============================================================================================================
+struct Error::Impl
+{
+   Impl() = default;
+
+   Impl(int in_errorCode, std::string in_name, ErrorLocation in_location) :
+      ErrorCode(in_errorCode),
+      Name(std::move(in_name)),
+      Location(std::move(in_location))
+   { };
+
+   Impl(int in_errorCode, std::string in_name, Error in_cause, ErrorLocation in_location) :
+      ErrorCode(in_errorCode),
+      Name(std::move(in_name)),
+      Cause(std::move(in_cause)),
+      Location(std::move(in_location))
+   { };
+
+   Impl(int in_errorCode, std::string in_name, std::string in_errorMessage, ErrorLocation in_location) :
+      ErrorCode(in_errorCode),
+      Name(std::move(in_name)),
+      Message(std::move(in_errorMessage)),
+      Location(std::move(in_location))
+   { };
+
+   Impl(int in_errorCode, std::string in_name, std::string in_errorMessage, Error in_cause, ErrorLocation in_location) :
+      ErrorCode(in_errorCode),
+      Name(std::move(in_name)),
+      Message(std::move(in_errorMessage)),
+      Cause(std::move(in_cause)),
+      Location(std::move(in_location))
+   { };
+
+   int ErrorCode;
+   std::string Name;
+   std::string Message;
+   Error Cause;
+   ErrorLocation Location;
+};
+
+std::ostream& operator<<(std::ostream& in_os, const Error& in_error)
+{
+   in_os << in_error.getSummary() << " at " << in_error.getLocation();
+   if (in_error.getCause())
+   {
+      in_os << std::endl << "   caused by: " << in_error.getCause();
+   }
+
+   return in_os;
+}
+
+Error::Error() :
+   m_impl(new Impl())
+{
+}
+
+Error::Error(Error&& in_other) noexcept :
+   m_impl(std::move(in_other.m_impl))
+{
+}
+
+Error::Error(const boost::system::error_code& in_ec, ErrorLocation in_errorLocation) :
+   m_impl(new Impl(in_ec.value(), in_ec.category().name(), std::move(in_errorLocation)))
+{
+}
+
+Error::Error(const boost::system::error_code& in_ec, Error in_cause, ErrorLocation in_errorLocation) :
+   m_impl(new Impl(in_ec.value(), in_ec.category().name(), std::move(in_cause), std::move(in_errorLocation)))
+{
+}
+
+Error::Error(const boost::system::error_code& in_ec, std::string in_errorMessage, ErrorLocation in_errorLocation) :
+   m_impl(new Impl(in_ec.value(), in_ec.category().name(), std::move(in_errorMessage), std::move(in_errorLocation)))
+{
+}
+
+Error::Error(
+   const boost::system::error_code& in_ec,
+   std::string in_errorMessage,
+   Error in_cause,
+   ErrorLocation in_errorLocation) :
+      m_impl(
+         new Impl(
+            in_ec.value(),
+            in_ec.category().name(),
+            std::move(in_errorMessage),
+            std::move(in_cause),
+            std::move(in_errorLocation)))
 {
 
 }
 
-Error::operator bool()
+Error::Error(
+   int in_errorCode,
+   std::string in_name,
+   ErrorLocation in_errorLocation) :
+      m_impl(new Impl(in_errorCode, std::move(in_name), std::move(in_errorLocation)))
 {
-   return 0 == m_errorCode;
 }
+
+Error::Error(
+   int in_errorCode,
+   std::string in_name,
+   Error in_cause,
+   ErrorLocation in_errorLocation) :
+      m_impl(
+         new Impl(
+            in_errorCode,
+            std::move(in_name),
+            std::move(in_cause),
+            std::move(in_errorLocation)))
+{
+}
+
+Error::Error(
+   int in_errorCode,
+   std::string in_name,
+   std::string in_errorMessage,
+   ErrorLocation in_errorLocation) :
+      m_impl(
+         new Impl(
+            in_errorCode,
+            std::move(in_name),
+            std::move(in_errorMessage),
+            std::move(in_errorLocation)))
+{
+}
+
+Error::Error(
+   int in_errorCode,
+   std::string in_name,
+   std::string in_errorMessage,
+   Error in_cause,
+   ErrorLocation in_errorLocation) :
+      m_impl(
+         new Impl(
+            in_errorCode,
+            std::move(in_name),
+            std::move(in_errorMessage),
+            std::move(in_cause),
+            std::move(in_errorLocation)))
+{
+}
+
+Error::operator bool() const
+{
+   return m_impl->ErrorCode == 0;
+}
+
+std::string Error::asString() const
+{
+   std::ostringstream oss;
+   oss << *this;
+   return oss.str();
+}
+
+std::string Error::getSummary() const
+{
+   std::ostringstream oss;
+   oss << m_impl->Name << " error " << m_impl->ErrorCode << " (" << m_impl->Message << ")";
+   return oss.str();
+}
+
+int Error::getErrorCode() const
+{
+   return m_impl->ErrorCode;
+}
+
+const std::string& Error::getName() const
+{
+   return m_impl->Name;
+}
+
+const std::string& Error::getMessage() const
+{
+   return m_impl->Message;
+}
+
+const Error& Error::getCause() const
+{
+   return m_impl->Cause;
+}
+
+const ErrorLocation& Error::getLocation() const
+{
+   return m_impl->Location;
+}
+
+// System Error ========================================================================================================
+Error systemError(int in_errorCode, ErrorLocation in_location)
+{
+   return std::move(
+      Error(
+         boost::system::error_code(
+            in_errorCode,
+            boost::system::system_category()),
+         std::move(in_location)));
+}
+
+Error systemError(int in_errorCode, Error in_cause, ErrorLocation in_location)
+{
+   return std::move(
+      Error(
+         boost::system::error_code(
+            in_errorCode,
+            boost::system::system_category()),
+         std::move(in_cause),
+         std::move(in_location)));
+}
+
+Error systemError(int in_errorCode, std::string in_errorMessage, ErrorLocation in_location)
+{
+   return std::move(
+      Error(
+         boost::system::error_code(
+            in_errorCode,
+            boost::system::system_category()),
+         std::move(in_errorMessage),
+         std::move(in_location)));
+}
+
+Error systemError(int in_errorCode, std::string in_errorMessage, Error in_cause, ErrorLocation in_location)
+{
+   return std::move(
+      Error(
+         boost::system::error_code(
+            in_errorCode,
+            boost::system::system_category()),
+         std::move(in_errorMessage),
+         std::move(in_cause),
+         std::move(in_location)));
+}
+
 
 } // namespace launcher_plugins
 } // namespace rstudio
