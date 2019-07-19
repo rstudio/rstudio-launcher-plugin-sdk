3792e22873528e58ee7265e4784cec70417023df
Maria Semple <maria@rstudio.com>
2019-07-18 21:14:45 -0700

    add a user class.

---
+++ sdk/include/system/User.hpp
@@ -0,0 +1,60 @@
+/*
+ * User.hpp
+ * 
+ * Copyright (C) 2019 by RStudio, Inc.
+ *
+ * TODO: License
+ *
+ */
+
+#ifndef LAUNCHER_PLUGINS_USER_HPP
+#define LAUNCHER_PLUGINS_USER_HPP
+
+#include <boost/noncopyable.hpp>
+
+#include <string>
+
+#include <PImpl.hpp>
+
+namespace rstudio {
+namespace launcher_plugins {
+namespace system {
+
+/**
+ * @brief Class which represents a system user.
+ */
+class User : boost::noncopyable
+{
+public:
+   /**
+    * @brief Default Constructor.
+    *
+    * Creates a user object which represents all users. It should be used for Launcher requests that apply to all users
+    * (e.g. get jobs for all users). Username == "*".
+    */
+   User();
+
+   /**
+    * @brief Creates a user by username.
+    *
+    * @param in_username    The name of the user.
+    */
+   explicit User(std::string in_username);
+
+   /**
+    * @brief Returns whether this object represents all users or not. See the default constructor for more details.
+    * @return
+    */
+   bool isAllUsers() const;
+
+   const std::string& getUsername() const;
+
+private:
+   PRIVATE_IMPL(m_impl);
+};
+
+} // namespace system
+} // namespace launcher_plugins
+} // namespace rstudio
+
+#endif
---
+++ sdk/src/system/User.cpp
@@ -0,0 +1,48 @@
+/*
+ * User.cpp
+ * 
+ * Copyright (C) 2019 by RStudio, Inc.
+ *
+ * TODO: License
+ *
+ */
+
+#include <system/User.hpp>
+
+namespace rstudio {
+namespace launcher_plugins {
+namespace system {
+
+struct User::Impl
+{
+   explicit Impl(std::string in_name) : Name(std::move(in_name)) {};
+
+   std::string Name;
+};
+
+PRIVATE_IMPL_DELETER_IMPL(User)
+
+User::User() :
+   m_impl(new Impl("*"))
+{
+}
+
+User::User(std::string in_username) :
+   m_impl(new Impl(std::move(in_username)))
+{
+}
+
+bool User::isAllUsers() const
+{
+   return m_impl->Name == "*";
+}
+
+const std::string& User::getUsername() const
+{
+   return m_impl->Name;
+}
+
+} // namespace system
+} // namespace launcher_plugins
+} // namespace rstudio
+
