15a3bc0e57b7cc0475062e8fc3ede9fda4722c06
Maria Semple <maria@rstudio.com>
2019-07-19 14:29:21 -0700

    changes to pinpl to reflect the real meaning of the different pointer types.

--- sdk/include/PImpl.hpp
+++ sdk/include/PImpl.hpp
@@ -16,32 +16,36 @@
  * @brief Shared start of the macro to define a private implementation for a class.
  */
 #define PRIVATE_IMPL_START                            \
-   struct Impl;                                       \
-   struct ImplDeleter { void operator()(Impl*); };
+   struct Impl;
 
 /**
  * @brief Macro to define a private implementation for a non-copyable class.
  *
- * This macro should be included in the private or protected section of a classes declaration.
- * The PRIVATE_IMPL_DELETER_IMPL macro must be used in the declaration file, after the definition of the Impl struct.
+ * Class which use this macro must either be non-copyable or define a custom deep-copy constructor and operator which
+ * performs a deep copy of the impl.
  *
- * @param in_memeberName    The name of the private implementation member variable (e.g. m_impl).
+ * This macro should be included in the private or protected section of a classes declaration.
+ * The PRIVATE_IMPL_DELETER_IMPL macro must be used in the definition file, after the definition of the Impl struct.
+ * struct OwningClass::Impl should be defined in the definition file before defining OwningClass.
+ *
+ * @param in_memberName    The name of the private implementation member variable (e.g. m_impl).
  */
-#define PRIVATE_IMPL(in_memberName)       \
-   PRIVATE_IMPL_START                     \
+#define PRIVATE_IMPL(in_memberName)                      \
+   PRIVATE_IMPL_START                                    \
+   struct ImplDeleter { void operator()(Impl*); };       \
    std::unique_ptr<Impl, ImplDeleter> in_memberName;
 
 /**
- * @brief Macro to define a private implementation for a copyable class.
+ * @brief Macro to define a private implementation which would be shared with copied instances.
  *
  * This macro should be included in the private or protected section of a classes declaration.
- * The PRIVATE_IMPL_DELETER_IMPL macro must be used in the declaration file, after the definition of the Impl struct.
+ * struct OwningClass::Impl should be defined in the definition file before defining OwningClass.
  *
  * @param in_memeberName    The name of the private implementation member variable (e.g. m_impl).
  */
-#define PRIVATE_IMPL_COPYABLE(in_memberName) \
+#define PRIVATE_IMPL_SHARED(in_memberName) \
    PRIVATE_IMPL_START                        \
-   std::shared_ptr<Impl, ImplDeleter> in_memberName;
+   std::shared_ptr<Impl> in_memberName;
 
 /**
  * @brief Macro which implements the deleter for the class's private implementation. This macro must be used after the
