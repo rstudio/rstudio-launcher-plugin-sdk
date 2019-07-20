efb84cbeaaf03258319a05d962dec46bd6c1ec32
Maria Semple <maria@rstudio.com>
2019-07-20 15:54:31 -0700

    add simple FilePath class.

---
+++ sdk/include/system/FilePath.hpp
@@ -0,0 +1,134 @@
+/*
+ * FilePath.hpp
+ * 
+ * Copyright (C) 2019 by RStudio, Inc.
+ *
+ * TODO: License
+ *
+ */
+
+#ifndef LAUNCHER_PLUGINS_FILE_PATH_HPP
+#define LAUNCHER_PLUGINS_FILE_PATH_HPP
+
+#include "Error.hpp"
+#include "PImpl.hpp"
+
+namespace rstudio {
+namespace launcher_plugins {
+namespace system {
+
+/**
+ * @brief Error codes for FilePath errors that are not from the system.
+ */
+enum class FilePathError
+{
+   WRONG_FILE_TYPE = 1
+};
+
+/**
+ * @brief Class which represents a path on the system. May be any type of file (e.g. directory, symlink, regular file,
+ *        etc.)
+ */
+class FilePath
+{
+public:
+   /**
+    * @brief Default constructor.
+    */
+   FilePath() = default;
+
+   /**
+    * @brief Constructor.
+    *
+    * @param in_path    The string representation of the path.
+    */
+   explicit FilePath(std::string in_path);
+
+   /**
+    * @brief Returns the absolute representation of this path as a string.
+    *
+    * @return The absolute representation of this path as a string.
+    */
+   std::string absolutePath() const;
+
+   /**
+    * @brief Creates a child path of this location using the relative path in_path.
+    *
+    * @param in_path    The relative path to create a child path of this path from.
+    *
+    * @return The child path.
+    */
+   FilePath childPath(const std::string& in_path) const;
+
+   /**
+    * @brief Attempts to create this directory, if it does not already exist.
+    *
+    * @return Success if the directory was created or already existed; system error otherwise (e.g. EPERM, ENOENT, etc.)
+    */
+   Error ensureDirectoryExists() const;
+
+   /**
+    * @brief Attempts to create this file, if it does not already exist.
+    *
+    * @return Success if the file was created or already existed; system error otherwise (e.g. EPERM, ENOENT, etc.)
+    */
+   Error ensureFileExists() const;
+
+   /**
+    * @brief Returns whether this path exists or not.
+    *
+    * @return True if this path points to a directory; false otherwise.
+    */
+   bool exists() const;
+
+   /**
+    * @brief Returns whether this path points to a directory or not.
+    *
+    * @return True if this path points to a directory; false otherwise.
+    */
+   bool isDirectory() const;
+
+   /**
+    * @brief Returns whether the FilePath is emtpy.
+    *
+    * @return True if the FilePath is empty, false otherwise.
+    */
+   bool isEmpty() const;
+
+   /**
+    * @brief Returns whether this path points to a regular file or not.
+    *
+    * @return True if this path points to a regular file; false otherwise.
+    */
+   bool isRegularFile() const;
+
+   /**
+    * @brief Opens this file for read.
+    *
+    * @param out_stream     The input stream for this open file.
+    *
+    * @return Success if the file was opened; system error otherwise (e.g. EPERM, ENOENT, etc.)
+    */
+   Error openForRead(std::shared_ptr<std::istream>& out_stream) const;
+
+
+   /**
+    * @brief Opens this file for write.
+    *
+    * @param out_stream     The output stream for this open file.
+    * @param in_truncate    Whether to truncate the existing contents of the file. Default: true.
+    *
+    * @return Success if the file was opened; system error otherwise (e.g. EPERM, ENOENT, etc.)
+    */
+   Error openForWrite(std::shared_ptr<std::ostream>& out_stream, bool in_truncate = true) const;
+
+private:
+   // Shared private implementation of the file path. This is safe because all methods of this class are const.
+   PRIVATE_IMPL_SHARED(m_impl);
+};
+
+} // namespace system
+} // namespace launcher_plugins
+} // namespace rstudio
+
+#endif
---
+++ sdk/src/system/FilePath.cpp
@@ -0,0 +1,236 @@
+/*
+ * FilePath.cpp
+ * 
+ * Copyright (C) 2019 by RStudio, Inc.
+ *
+ * TODO: License
+ *
+ */
+
+#include "system/FilePath.hpp"
+
+#include <boost/filesystem.hpp>
+
+#include "logging/Logger.hpp"
+
+using namespace rstudio::launcher_plugins::logging;
+
+namespace rstudio {
+namespace launcher_plugins {
+namespace system {
+
+namespace {
+
+inline Error convertFilesystemError(const boost::filesystem::filesystem_error& in_e, ErrorLocation in_location)
+{
+
+   std::string message = in_e.what();
+   if (!in_e.path1().empty())
+   {
+      message += "\n    Path: " + in_e.path1().string();
+      if (!in_e.path2().empty())
+         message += "\n    Path 2: " + in_e.path2().string();
+   }
+
+   return Error(in_e.code(), message, std::move(in_location));
+}
+
+} // anonymous namespace
+
+typedef boost::filesystem::path PathType;
+
+struct FilePath::Impl
+{
+   Impl() {};
+
+   Impl(PathType in_path) : Path(std::move(in_path)) { };
+
+   PathType Path;
+};
+
+FilePath::FilePath(std::string in_path) :
+   m_impl(new Impl(std::move(in_path)))
+{
+}
+
+std::string FilePath::absolutePath() const
+{
+   if (isEmpty())
+      return "";
+   else
+      return m_impl->Path.generic_string();
+}
+
+FilePath FilePath::childPath(const std::string& in_path) const
+{
+   if (!isEmpty())
+   {
+      try
+      {
+         PathType childPath(in_path);
+         if (childPath.has_root_path())
+            throw boost::filesystem::filesystem_error(
+               "absolute path not permitted",
+               childPath,
+               boost::system::error_code(
+                  boost::system::errc::no_such_file_or_directory,
+                  boost::system::system_category()));
+
+         FilePath childFilePath;
+         childFilePath.m_impl->Path = boost::filesystem::absolute(childPath, m_impl->Path);
+         return childFilePath;
+      }
+      catch (boost::filesystem::filesystem_error& e)
+      {
+         logging::logError(convertFilesystemError(e, ERROR_LOCATION));
+      }
+   }
+
+   return *this;
+}
+
+Error FilePath::ensureDirectoryExists() const
+{
+   if (!exists())
+   {
+      try
+      {
+         boost::filesystem::create_directory(m_impl->Path);
+      }
+      catch (boost::filesystem::filesystem_error& e)
+      {
+         return convertFilesystemError(e, ERROR_LOCATION);
+      }
+   }
+
+   return Success();
+}
+
+Error FilePath::ensureFileExists() const
+{
+   if (!exists())
+   {
+      std::shared_ptr<std::ostream> stream;
+      Error error = openForWrite(stream);
+      if (error)
+         return error;
+
+      // Close the file handle.
+      stream->flush();
+      stream.reset();
+   }
+
+   return Success();
+}
+
+bool FilePath::exists() const
+{
+   try
+   {
+      if (!m_impl->Path.empty())
+         return boost::filesystem::exists(m_impl->Path);
+   }
+   catch (const boost::filesystem::filesystem_error& e)
+   {
+      logging::logError(convertFilesystemError(e, ERROR_LOCATION));
+   }
+
+   return false;
+}
+
+bool FilePath::isEmpty() const
+{
+   return m_impl->Path.empty();
+}
+
+bool FilePath::isRegularFile() const
+{
+   try
+   {
+      if (exists())
+         return boost::filesystem::is_regular_file(m_impl->Path);
+   }
+   catch (const boost::filesystem::filesystem_error& e)
+   {
+      logError(convertFilesystemError(e, ERROR_LOCATION));
+   }
+
+   return false;
+}
+
+bool FilePath::isDirectory() const
+{
+   try
+   {
+      if (exists())
+         return boost::filesystem::is_directory(m_impl->Path);
+   }
+   catch (const boost::filesystem::filesystem_error& e)
+   {
+      logError(convertFilesystemError(e, ERROR_LOCATION));
+   }
+
+   return false;
+}
+
+Error FilePath::openForRead(std::shared_ptr<std::istream>& out_stream) const
+{
+   try
+   {
+      std::shared_ptr<std::ifstream> stream(
+         new std::ifstream(absolutePath().c_str(), std::ios_base::in | std::ios_base::binary));
+      if (!(*stream))
+      {
+         return systemError(
+            boost::system::errc::no_such_file_or_directory,
+            "Unable to open file for read: " + absolutePath(),
+            ERROR_LOCATION);
+      }
+
+      out_stream = stream;
+   }
+   catch (const std::exception& e)
+   {
+      std::string message = e.what();
+      message += "\n    Path: " + absolutePath();
+      return systemError(boost::system::errc::io_error, message, ERROR_LOCATION);
+   }
+
+   return Success();
+}
+
+Error FilePath::openForWrite(std::shared_ptr<std::ostream>& out_stream, bool in_truncate) const
+{
+   try
+   {
+      std::ios_base::openmode flags =
+         std::ios_base::out |
+         std::ios_base::binary |
+         (in_truncate ? std::ios_base::trunc : std::ios_base::app);
+      std::shared_ptr<std::ostream> stream(new std::ofstream(absolutePath().c_str(), flags));
+
+      if (!*stream)
+      {
+         return systemError(
+            boost::system::errc::no_such_file_or_directory,
+            "Unable to open file for write: " + absolutePath(),
+            ERROR_LOCATION);
+      }
+
+      out_stream = stream;
+   }
+   catch (const std::exception& e)
+   {
+      std::string message = e.what();
+      message += "\n    Path: " + absolutePath();
+      return systemError(boost::system::errc::io_error, message, ERROR_LOCATION);
+   }
+
+
+   return Success();
+}
+
+} // namespace system
+} // namespace launcher_plugins
+} // namespace rstudio
+
