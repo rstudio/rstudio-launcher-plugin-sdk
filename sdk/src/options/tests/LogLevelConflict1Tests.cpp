af4bc5bbe07420c799c438eefa93eb2c209c4eff
Maria Semple <maria@rstudio.com>
2019-08-12 11:02:21 -0700

    add options unit tests

---
+++ sdk/src/options/tests/LogLevelConflict1Tests.cpp
@@ -0,0 +1,60 @@
+/*
+ * LogLevelConflict1Tests.cpp
+ * 
+ * Copyright (C) 2019 by RStudio, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *
+ */
+
+#include "../../tests/TestMain.hpp"
+
+#include <boost/thread.hpp>
+
+#include "options/Options.hpp"
+#include "system/FilePath.hpp"
+#include "system/User.hpp"
+
+namespace rstudio {
+namespace launcher_plugins {
+namespace options {
+
+TEST_CASE("log level conflict (less detail) options")
+{
+   SECTION("read options")
+   {
+      constexpr const char* argv[] = {};
+      constexpr int argc = 0;
+
+      Options& opts = Options::getInstance();
+      Error error = opts.readOptions(argc, argv, system::FilePath("./conf-files/LogLevelConflict1.conf"));
+      REQUIRE(!error);
+   }
+
+   SECTION("check values")
+   {
+      Options& opts = Options::getInstance();
+      REQUIRE(opts.getJobExpiryHours() == 24);
+      REQUIRE(opts.getHeartbeatIntervalSeconds() == 5);
+      REQUIRE(opts.getLogLevel() == logging::LogLevel::DEBUG);
+      REQUIRE(opts.getScratchPath().absolutePath() == "/var/lib/rstudio-launcher/");
+      REQUIRE(opts.getServerUser().getUsername() == "rstudio-server");
+      REQUIRE(opts.getThreadPoolSize() == std::max<unsigned int>(4, boost::thread::hardware_concurrency()));
+   }
+}
+
+} // namespace options
+} // namespace launcher_plugins
+} // namespace rstudio
+
