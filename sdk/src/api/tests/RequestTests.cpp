002c5a6588c7d1a89f376629100e389a61a998e8
Maria Semple <maria@rstudio.com>
2020-01-21 18:17:22 -0800

    adds request test cases for existing request types

--- sdk/src/api/tests/RequestTests.cpp
+++ sdk/src/api/tests/RequestTests.cpp
@@ -25,10 +25,108 @@
 
 #include <Error.hpp>
 #include <api/Request.hpp>
+#include <json/Json.hpp>
 
 namespace rstudio {
 namespace launcher_plugins {
 namespace api {
+
+TEST_CASE("Parse valid bootstrap request")
+{
+   json::Object versionObj;
+   versionObj.insert("major", json::Value(2));
+   versionObj.insert("minor", json::Value(11));
+   versionObj.insert("patch", json::Value(375));
+
+   json::Object requestObj;
+   requestObj.insert("version", versionObj);
+   requestObj.insert("messageType", json::Value(static_cast<int>(Request::Type::BOOTSTRAP)));
+   requestObj.insert("requestId", json::Value(6));
+
+   std::shared_ptr<Request> request;
+   Error error = Request::fromJson(requestObj, request);
+
+   REQUIRE_FALSE(error);
+   REQUIRE(request != nullptr);
+   CHECK(request->getType() == Request::Type::BOOTSTRAP);
+   CHECK(request->getId() == 6);
+
+   BootstrapRequest* bootstrapRequest = dynamic_cast<BootstrapRequest*>(request.get());
+   REQUIRE(bootstrapRequest != nullptr);
+   CHECK(bootstrapRequest->getMajorVersion() == 2);
+   CHECK(bootstrapRequest->getMinorVersion() == 11);
+   CHECK(bootstrapRequest->getPatchNumber() == 375);
+}
+
+TEST_CASE("Parse invalid bootstrap request")
+{
+   json::Object versionObj;
+   versionObj.insert("major", json::Value(2));
+   versionObj.insert("patch", json::Value(375));
+
+   json::Object requestObj;
+   requestObj.insert("version", versionObj);
+   requestObj.insert("messageType", json::Value(static_cast<int>(Request::Type::BOOTSTRAP)));
+   requestObj.insert("requestId", json::Value(6));
+
+   std::shared_ptr<Request> request;
+   Error error = Request::fromJson(requestObj, request);
+
+   REQUIRE(error);
+   REQUIRE(error.getMessage().find("minor") != std::string::npos);
+}
+
+TEST_CASE("Parse invalid request - missing message type")
+{
+   json::Object requestObj;
+   requestObj.insert("requestId", json::Value(6));
+
+
+   std::shared_ptr<Request> request;
+   Error error = Request::fromJson(requestObj, request);
+
+   REQUIRE(error);
+   REQUIRE(error.getMessage().find("messageType") != std::string::npos);
+}
+
+TEST_CASE("Parse invalid request - missing request request ID")
+{
+   json::Object requestObj;
+   requestObj.insert("messageType", json::Value(static_cast<int>(Request::Type::BOOTSTRAP)));
+
+
+   std::shared_ptr<Request> request;
+   Error error = Request::fromJson(requestObj, request);
+
+   REQUIRE(error);
+   REQUIRE(error.getMessage().find("requestId") != std::string::npos);
+}
+
+TEST_CASE("Parse invalid request - negative message type")
+{
+   json::Object requestObj;
+   requestObj.insert("messageType", json::Value(-4));
+   requestObj.insert("requestId", json::Value(6));
+
+   std::shared_ptr<Request> request;
+   Error error = Request::fromJson(requestObj, request);
+
+   REQUIRE(error);
+   REQUIRE(error.getMessage().find("-4") != std::string::npos);
+}
+
+TEST_CASE("Parse invalid request - message type too large")
+{
+   json::Object requestObj;
+   requestObj.insert("messageType", json::Value(568));
+   requestObj.insert("requestId", json::Value(6));
+
+   std::shared_ptr<Request> request;
+   Error error = Request::fromJson(requestObj, request);
+
+   REQUIRE(error);
+   REQUIRE(error.getMessage().find("568") != std::string::npos);
+}
 
 } // namespace api
 } // namespace launcher_plugins
