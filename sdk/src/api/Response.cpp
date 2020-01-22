6da9b5f244187cf2ec1caeafd56865bdee688b76
Maria Semple <maria@rstudio.com>
2020-01-22 14:57:08 -0800

    makes response IDs start at 1 since 0 is reserved for heartbeats

--- sdk/src/api/Response.cpp
+++ sdk/src/api/Response.cpp
@@ -61,7 +61,7 @@ struct Response::Impl
    const uint64_t ResponseId;
 };
 
-std::atomic_uint64_t Response::Impl::NEXT_RESPONSE_ID { 0 };
+std::atomic_uint64_t Response::Impl::NEXT_RESPONSE_ID { 1 };
 
 PRIVATE_IMPL_DELETER_IMPL(Response)
 
