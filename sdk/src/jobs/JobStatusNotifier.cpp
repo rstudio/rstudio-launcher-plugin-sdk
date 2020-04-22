02ec14e99c7900c099d3ce4997a43582c0f13e59
Maria Semple <maria@rstudio.com>
2020-04-22 11:14:10 -0700

    add Subscription impl

--- sdk/src/jobs/JobStatusNotifier.cpp
+++ sdk/src/jobs/JobStatusNotifier.cpp
@@ -24,9 +24,101 @@
 
 #include <jobs/JobStatusNotifier.hpp>
 
+#include <mutex>
+
+#include <boost/signals2.hpp>
+
+#include <utils/MutexUtils.hpp>
+
 namespace rstudio {
 namespace launcher_plugins {
 namespace jobs {
+
+namespace {
+
+// Convenience typedefs for self.
+typedef std::shared_ptr<JobStatusNotifier> SharedThis;
+typedef std::shared_ptr<JobStatusNotifier> Parent;
+typedef std::weak_ptr<JobStatusNotifier> WeakParent;
+
+// Convenience typedefs for signals.
+typedef boost::signals2::connection Connection;
+typedef boost::signals2::signal<void(api::JobPtr)> Signal;
+typedef std::map<std::string, Signal> SignalMap;
+
+} // anonymous namespace
+
+/**
+ * @brief JobStatusNotifier implementation.
+ */
+struct JobStatusNotifier::Impl
+{
+   /** Signal to be used when something subscribes to all jobs. */
+   Signal AllJobsSignal;
+
+   /** Map of every signal per job. */
+   SignalMap JobSignalMap;
+
+   /** Mutex. */
+   std::mutex Mutex;
+};
+
+PRIVATE_IMPL_DELETER_IMPL(JobStatusNotifier)
+
+/**
+ * @brief A class which represents a subscription to one or more jobs.
+ */
+struct Subscription
+{
+   /**
+    * @brief Constructor.
+    *
+    * @param in_parent          The JobStatusNotifier which created this subscription.
+    * @param in_jobId           The ID of the job  for which this subscription was created.
+    * @param in_connection      The boost signal connection.
+    */
+   Subscription(WeakParent in_parent, std::string in_jobId, Connection in_connection) :
+      m_parent(std::move(in_parent)),
+      m_jobId(std::move(in_jobId)),
+      m_connection(std::move(in_connection))
+   {
+   }
+
+   /**
+    * @brief Destructor.
+    */
+   ~Subscription()
+   {
+      if (Parent parent = m_parent.lock())
+      {
+         m_connection.disconnect();
+
+         if (!m_jobId.empty())
+         {
+            UNIQUE_LOCK_MUTEX(parent->m_impl->Mutex)
+            {
+               auto itr = parent->m_impl->JobSignalMap.find(m_jobId);
+               if (itr != parent->m_impl->JobSignalMap.end() && itr->second.empty())
+               {
+                  parent->m_impl->JobSignalMap.erase(itr);
+               }
+            }
+            END_LOCK_MUTEX
+         }
+      }
+   }
+
+private:
+   /** The JobStatusNotifier which created this subscription. */
+   WeakParent m_parent;
+
+   /** The ID of the job  for which this subscription was created. */
+   std::string m_jobId;
+
+   /** The boost signal connection. */
+   Connection m_connection;
+};
+
 
 
 } // namespace jobs
